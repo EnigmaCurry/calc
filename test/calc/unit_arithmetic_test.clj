8e9aa844ae70701305b37b1ef66cd421a97d7f54
EnigmaCurry <ryan@enigmacurry.com>
2026-05-30 07:37:24 +0000

    stuff

--- test/calc/unit_arithmetic_test.clj
+++ test/calc/unit_arithmetic_test.clj
@@ -70,6 +70,38 @@
       ;; Should produce something like "4.125... days" or "99.167... hours"
       (is (not (clojure.string/includes? result "Error"))))))
 
+(deftest evaluates-multiplication
+  (testing "60 mph * 2 hours in miles = 120"
+    (let [{:keys [result target]} (cli/process-request-text "60 mph * 2 hours in miles" nil)]
+      (is (= "120" result))
+      (is (= "mi" target))))
+
+  (testing "5 meters * 3 meters in sq meters"
+    (let [{:keys [result target]} (cli/process-request-text "5 meters * 3 meters in sq meters" nil)]
+      (is (= "15" result))
+      (is (= "m^2" target))))
+
+  (testing "60 mph * 2 hours auto-selects unit"
+    (let [{:keys [result]} (cli/process-request-text "60 mph * 2 hours" nil)]
+      (is (some? result))
+      (is (not (clojure.string/includes? result "Error"))))))
+
+(deftest evaluates-division
+  (testing "100 MB / 100 Mbps in seconds = 8"
+    (let [{:keys [result target]} (cli/process-request-text "100 MB / 100 Mbps in seconds" nil)]
+      (is (= "8" result))
+      (is (= "s" target))))
+
+  (testing "100 km / 2 hours in km/hr = 50"
+    (let [{:keys [result target]} (cli/process-request-text "100 km / 2 hours in km/hr" nil)]
+      (is (= "50" result))
+      (is (= "km/hr" target))))
+
+  (testing "100 MB / 100 Mbps auto-selects unit"
+    (let [{:keys [result]} (cli/process-request-text "100 MB / 100 Mbps" nil)]
+      (is (some? result))
+      (is (not (clojure.string/includes? result "Error"))))))
+
 (deftest rejects-incompatible-unit-addition
   (testing "1 hour + 5 meters is incompatible"
     (let [{:keys [error]} (cli/process-request-text "1 hour + 5 meters in seconds" nil)]
