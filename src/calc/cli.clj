c099de57d1914edee3a4f340404094bc67cd9d00
EnigmaCurry <ryan@enigmacurry.com>
2026-06-01 04:50:14 +0000

    use MENU_COMPLETE for interactive overlay instead of printed list

--- src/calc/cli.clj
+++ src/calc/cli.clj
@@ -422,12 +422,13 @@
                      (.completer (make-completer))
                      (.highlighter (make-preview-highlighter fmt-opts accepting))
                      (.build))]
-    ;; Enable grouped menu completion
+    ;; MENU_COMPLETE: Tab cycles through an interactive menu overlay (never prints
+    ;; a static list into scrollback).  The menu is dismissed cleanly on Enter.
+    (.setOpt reader LineReader$Option/MENU_COMPLETE)
     (.setOpt reader LineReader$Option/AUTO_LIST)
     (.setOpt reader LineReader$Option/AUTO_MENU)
     (.setOpt reader LineReader$Option/GROUP)
     (.setOpt reader LineReader$Option/LIST_ROWS_FIRST)
-    (.setOpt reader LineReader$Option/ERASE_LINE_ON_FINISH)
     (.setVariable reader LineReader/HISTORY_FILE hist-path)
     (let [widgets (.getWidgets reader)
           ^Widget orig-accept (get widgets LineReader/ACCEPT_LINE)]
@@ -435,8 +436,6 @@
         (reify Widget
           (apply [_]
             (reset! accepting true)
-            ;; Clear completion menu before accepting
-            (.callWidget reader LineReader/CLEAR)
             (.apply orig-accept)))))
     (println "calc — type '/help' for usage, Ctrl-D to exit")
     (loop []
